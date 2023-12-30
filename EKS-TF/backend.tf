3e6624f5f9bdb30712dee5f71b8479d402f424e3
Blessed Uyo <blessed.uyo.ext@jumia.com>
2023-12-30 05:50:45 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "bleszily" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "bleszily" # Replace with your actual S3 bucket name
+#    key    = "EKS/terraform.tfstate"
+#    region = "eu-west-2"
+#  }
+#}
