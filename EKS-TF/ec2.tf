b7a5fc458a986771f5d5379ca2ca097a41348d7b
Blessed Uyo <blessed.uyo.ext@jumia.com>
2023-12-30 05:50:16 +0000

    Update ec2.tf

--- EKS-TF/ec2.tf
+++ EKS-TF/ec2.tf
@@ -1,22 +1,22 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0bff5d6c5f5b6d1e1" 
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Example"
-  }
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id] 
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0bff5d6c5f5b6d1e1" 
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "Example"
+#  }
+#
+#  vpc_security_group_ids = [aws_security_group.allow_ssh.id] 
+#}
+#
+#resource "aws_security_group" "allow_ssh" {
+#  name        = "allow_ssh"
+#  description = "Allow SSH inbound traffic"
+#
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
